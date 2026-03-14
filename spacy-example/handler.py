acaf4d18670d6024af9677736b9c3ffd41a9e24d
barneyn1 <89147297+barneyn1@users.noreply.github.com>
2026-03-14 11:11:01 -0400

    Update handler.py spacy-example

--- spacy-example/handler.py
+++ spacy-example/handler.py
@@ -1,50 +1,28 @@
-print('container start')
-
-try:
-    import unzip_requirements
-except ImportError as error:
-    pass
-
-print('unzipped')
-
 import json
 import en_core_web_sm
 
 MODEL = en_core_web_sm.load()
-print('model loaded')
 
 def create_ner_spans(text):
     doc = MODEL(text)
-    spans = []
-    for ent in doc.ents:
-        span = {
-            'start': ent.start_char,
-            'end': ent.end_char,
-            'type': ent.label_
+    return [
+        {
+            "start": ent.start_char,
+            "end": ent.end_char,
+            "type": ent.label_
         }
-        spans.append(span)
-    return spans
-
+        for ent in doc.ents
+    ]
 
 def handle_request(event, context):
-    text = event['body']
-    print(text)
-    spans = []
-    if text is not None:
-        spans = create_ner_spans(text)
-    print(spans)
+    text = event.get("body")
+    spans = create_ner_spans(text) if text else []
 
-    body = {
-        'spans': spans
-    }
-
-    response = {
+    return {
         "statusCode": 200,
-        "body": json.dumps(body),
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+        "body": json.dumps({"spans": spans}),
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         }
     }
-
-    return response
