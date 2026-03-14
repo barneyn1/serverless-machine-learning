ab5899deb9ca9b36cafd785d9f37046b7fd281be
barneyn1 <89147297+barneyn1@users.noreply.github.com>
2026-03-14 11:11:55 -0400

    Update handler.py spacy-assignment

--- spacy-assignment/handler.py
+++ spacy-assignment/handler.py
@@ -1,41 +1,23 @@
-print('container start')
-try:
-  import unzip_requirements
-except ImportError:
-  pass
-print('unzipped')
-
 import json
 import en_core_web_sm
 from spacy import displacy
-print('imports end')
 
 MODEL = en_core_web_sm.load()
-print('model loaded')
 
 def tag_and_parse(text):
     doc = MODEL(text)
     return displacy.parse_deps(doc)
 
 def handle_request(event, context):
-    request_body = event['body']
-    text = json.loads(request_body)['text']
-    print(text)
-    
-    response_body = {
-        'words': [],
-        'arcs': []
-    }
+    request_body = event.get("body")
+    text = json.loads(request_body)["text"] if request_body else None
+    response_body = tag_and_parse(text) if text else {"words": [], "arcs": []}
 
-    if text is not None:
-        response_body = tag_and_parse(text)
-    
-    response = {
+    return {
         "statusCode": 200,
         "body": json.dumps(response_body),
-        "headers" : {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         }
     }
-    return response
